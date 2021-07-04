4f6a0d7ed55c6148ddc1db6daef0006f28418934
testhdm <83900038+testhdm@users.noreply.github.com>
2021-07-04 12:22:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 """
-If you using VPS, first fork repo, remove upper strings and edit like this:
-
-BOTTOKEN = "1357892639:AAFz-ke7RIJmLcaO31ZKz3CmsZaDmS0STkc" # your bot token frpm @BotFather
-APIID = "1795096" # your api id from https://my.telegram.org/
-APIHASH = "5ae7ae34532c5596aa1a43c51d92c04c" # your api hash from https://my.telegram.org/
-DOWNLOADPATH = "Downloads/" # don't change
-USERNAME = "@hedwigdittomaltruvantbot" # put your bot username here
+BOTTOKEN = "1357892639:AAFz-ke7RIJmLcaO31ZKz3CmsZaDmS0STkc"
+APIID = 1795096
+APIHASH = "5ae7ae34532c5596aa1a43c51d92c04c"
+DOWNLOADPATH = "Downloads/"
+USERNAME = "@hedwigdittomaltruvantbot"
 """
