f8451e956a967de2005380f10e45ad869ffa7b30
testhdm <83900038+testhdm@users.noreply.github.com>
2021-07-04 12:09:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,18 +3,12 @@
 
 import os
 
-BOTTOKEN = os.environ.get("BOTTOKEN", "1357892639:AAFz-ke7RIJmLcaO31ZKz3CmsZaDmS0STkc")
-APIID = int(os.environ.get("APIID", "1795096"))
-APIHASH = os.environ.get("APIHASH", "5ae7ae34532c5596aa1a43c51d92c04c")
-DOWNLOADPATH = os.environ.get("DOWNLOADPATH", "Downloads/")
-USERNAME = os.environ.get("USERNAME", "@hedwigdittomaltruvantbot")
-
 """
 If you using VPS, first fork repo, remove upper strings and edit like this:
 
-BOTTOKEN = "" # your bot token frpm @BotFather
-APIID =      # your api id from https://my.telegram.org/
-APIHASH = "" # your api hash from https://my.telegram.org/
+BOTTOKEN = "1357892639:AAFz-ke7RIJmLcaO31ZKz3CmsZaDmS0STkc" # your bot token frpm @BotFather
+APIID = "1795096" # your api id from https://my.telegram.org/
+APIHASH = "5ae7ae34532c5596aa1a43c51d92c04c" # your api hash from https://my.telegram.org/
 DOWNLOADPATH = "Downloads/" # don't change
-USERNAME = "" # put your bot username here
+USERNAME = "@hedwigdittomaltruvantbot" # put your bot username here
 """
