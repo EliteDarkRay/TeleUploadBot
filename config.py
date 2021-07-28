64ff8b63e2539ebbb54df706762a74d293e1a34b
EliteDarkRay <71128663+EliteDarkRay@users.noreply.github.com>
2021-07-28 19:36:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
-"""
-BOTTOKEN = "1357892639:AAFz-ke7RIJmLcaO31ZKz3CmsZaDmS0STkc"
-APIID = 1795096
-APIHASH = "5ae7ae34532c5596aa1a43c51d92c04c"
-DOWNLOADPATH = "Downloads/"
-USERNAME = "@hedwigdittomaltruvantbot"
-"""
+BOTTOKEN = os.environ.get("BOTTOKEN", "1357892639:AAFz-ke7RIJmLcaO31ZKz3CmsZaDmS0STkc")
+APIID = int(os.environ.get("APIID", 1795096))
+APIHASH = os.environ.get("APIHASH", "5ae7ae34532c5596aa1a43c51d92c04c")
+DOWNLOADPATH = os.environ.get("DOWNLOADPATH", "Downloads/")
+USERNAME = os.environ.get("USERNAME", "@hedwigdittomaltruvantbot")
