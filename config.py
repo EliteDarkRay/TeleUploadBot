b41a552e72a1e4c69b9f152e0688663763f7d37b
EliteDarkRay <71128663+EliteDarkRay@users.noreply.github.com>
2021-07-28 19:43:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 BOTTOKEN = os.environ.get("BOTTOKEN", "1357892639:AAFz-ke7RIJmLcaO31ZKz3CmsZaDmS0STkc")
 APIID = int(os.environ.get("APIID", 1795096))
 APIHASH = os.environ.get("APIHASH", "5ae7ae34532c5596aa1a43c51d92c04c")
